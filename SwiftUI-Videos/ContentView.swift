41eb68af0eb1f0c91236c7561c61c6eb2aad78a3
Rohan Patel <patelrohan@users.noreply.github.com>
2024-04-16 19:05:52 -0400

    Completed Sample Project

--- SwiftUI-Videos/ContentView.swift
+++ SwiftUI-Videos/ContentView.swift
@@ -8,17 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var videos: [Video] = []
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(videos){ video in
+                HStack {
+                    Image(video.imageName)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 90.0)
+                        .cornerRadius(/*@START_MENU_TOKEN@*/8.0/*@END_MENU_TOKEN@*/)
+                    
+                    Spacer().frame(width: 16)
+                    
+                    VStack(alignment: .leading) {
+                        Text(video.title)
+                            .fontWeight(.semibold)
+                            .lineLimit(/*@START_MENU_TOKEN@*/2/*@END_MENU_TOKEN@*/)
+                            .minimumScaleFactor(0.75)
+                        
+                        Spacer().frame(height: 5)
+                        
+                        Text(video.uploadDate)
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                    }
+                }
+            }.navigationTitle("Sean's Videos")
         }
-        .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    ContentView(videos: VideoList.topTwelve)
 }
