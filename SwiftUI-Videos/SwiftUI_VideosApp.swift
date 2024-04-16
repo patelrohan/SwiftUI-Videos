fe5dbeb9581e106c9a3aa52be289e74c02c15b04
Rohan Patel <patelrohan@users.noreply.github.com>
2024-04-15 21:04:28 -0400

    Initial Commit

---
+++ SwiftUI-Videos/SwiftUI_VideosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_VideosApp.swift
+//  SwiftUI-Videos
+//
+//  Created by Rohan on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_VideosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
